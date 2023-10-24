776fe427a540a64c2233f63a4177463049f6ea13
benharrisss <146212225+benharrisss@users.noreply.github.com>
2023-10-24 15:44:38 +0000

    Week3-C

--- week3_c_bootcamp/worksheet_1/updatefile.c
+++ week3_c_bootcamp/worksheet_1/updatefile.c
@@ -2,26 +2,29 @@
 #include <stdlib.h>
 
 int main(){
-    char filename [] = "squares.dat";
-    FILE *file = fopen (filename, "r");
+    char filename [] = "numbers.dat";
+    FILE *file = fopen (filename, "a+");
     if (file == NULL) {
         perror("");
         return 1;
     }
+
     float counter = 0;
     float total = 0;
-    int num = 0;
+    float num = 0;
     int buffer_size = 100;
     char line_buffer[buffer_size];
     while (fgets(line_buffer, buffer_size, file) != NULL){
-        int num = atoi(line_buffer);
-        printf("%d\n", num);
+        float num = atof(line_buffer);
+        printf("%f\n", num);
         total = total + num;
         counter = counter + 1;
     }
     float average = (total / counter);
-    printf("The avergae is %f\n", average);
+    fprintf(file, "%f\n", (average));
+    printf("%f\n", average);
+
 
     fclose(file);
     return 0;
-}    
+}
